d69d661c31d8e7e350018634620a434cc505d7cd
namnt1 <namnt1@smartosc.com>
2022-11-03 15:38:56 +0700

    commit code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,14 +4,15 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
-
     public static void main(String[] args) {
         Main main = new Main();
         main.validEmail("email@subdomain.domain.com");
     }
 
     public boolean validEmail(String email){
-        String regexPattern = "^(?=.{1,64}@)([^0-9!@#$+._\"-]{0,})@([a-zA-Z]{0,})[.]([com]{1,})";
+        String domain = "com";
+        String regexPattern = "^[^#@\\d.][a-zA-Z]*@(?:[a-zA-Z0-9])+[*.]" + domain + "$";
+//        String regexPattern = "(^[^0-9!@#$+._ \"-]{0,})@([a-zA-Z]{0,})[.]([com]{1,})";
         Pattern pattern = Pattern.compile(regexPattern);
         Matcher matcher = pattern.matcher(email);
         return matcher.find();
--- src/com/company/MainTest.java
+++ src/com/company/MainTest.java
@@ -16,35 +16,35 @@ public class MainTest {
     @Parameterized.Parameters
     public static Collection data(){
         return Arrays.asList(new Object[][] {
-                {"email@domain.com", true},
-                {"firstname.lastname@domain.com",false},
-                {"email@subdomain.domain.com",false},
-                {"firstname+lastname@domain.com",false},
-                {"email@123.123.123.123",false},
-                {"email@[123.123.123.123]",false},
-                {"\"email\"@domain.com",false},
-                {"1234567890@domain.com",false},
-                {"email@domain-one.com",false},
-                {"_______@domain.com",false},
-                {"email@domain.name",false},
-                {"email@domain.co.jp",false},
-                {"firstname-lastname@domain.com",false},
-                {"plainaddress",false},
-                {"#@%^%#$@#$@#.com",false},
-                {"@domain.com",false},
-                {"Joe Smith <email@domain.com>",false},
-                {"email.domain.com",false},
-                {"email@domain@domain.com",false},
-                {".email@domain.com",false},
-                {"email.@domain.com",false},
-                {"email..email@domain.com",false},
-                {"あいうえお@domain.com",false},
-                {"email@domain.com (Joe Smith)",false},
-                {"email@domain",false},
-                {"email@-domain.com",false},
-                {"email@domain.web",false},
-                {"email@111.222.333.44444",false},
-                {"email@domain..com",false},
+                {"email@domain.com", true},//0
+                {"firstname.lastname@domain.com",false},//1
+                {"email@subdomain.domain.com",false},//2
+                {"firstname+lastname@domain.com",false},//3
+                {"email@123.123.123.123",false},//4
+                {"email@[123.123.123.123]",false},//5
+                {"\"email\"@domain.com",false},//6
+                {"1234567890@domain.com",false},//7
+                {"email@domain-one.com",false},//8
+                {"_______@domain.com",false},//9
+                {"email@domain.name",false},//10
+                {"email@domain.co.jp",false},//11
+                {"firstname-lastname@domain.com",false},//12
+                {"plainaddress",false},//13
+                {"#@%^%#$@#$@#.com",false},//14
+                {"@domain.com",false},//15
+                {"Joe Smith <email@domain.com>",false},//16
+                {"email.domain.com",false},//17
+                {"email@domain@domain.com",false},//18
+                {".email@domain.com",false},//19
+                {"email.@domain.com",false},//20
+                {"email..email@domain.com",false},//21
+                {"あいうえお@domain.com",false},//22
+                {"email@domain.com (Joe Smith)",false},//23
+                {"email@domain",false},//24
+                {"email@-domain.com",false},//25
+                {"email@domain.web",false},//26
+                {"email@111.222.333.44444",false},//27
+                {"email@domain..com",false},//28
         });
     }
 
